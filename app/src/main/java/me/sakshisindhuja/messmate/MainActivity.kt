b7ea0dac17ce271b8f1bab325ea57127e290243b
Sakshi Sindhuja <sakshisindhuja.01@gmail.com>
2022-05-25 02:27:28 +0530

    Navigating to a complaints screen
    Data view is cleaner, and an action button sends us to a form input screen.

--- app/src/main/java/me/sakshisindhuja/messmate/MainActivity.kt
+++ app/src/main/java/me/sakshisindhuja/messmate/MainActivity.kt
@@ -1,8 +1,10 @@
 package me.sakshisindhuja.messmate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 import me.sakshisindhuja.messmate.datamodels.TestDataModel
 
@@ -17,5 +19,12 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.meals_list_recycler_view)
         recyclerView.adapter = MealsListAdapter(this, myDataset)
 
+        val reportingActionButton = findViewById<FloatingActionButton>(R.id.report_action_button)
+        reportingActionButton.setOnClickListener {
+            val context = reportingActionButton.context
+            val intent = Intent(context, ReportingActivity::class.java)
+            intent.putExtra("currentDate", "26/05/2022")
+            context.startActivity(intent)
+        }
     }
 }
--- app/src/main/java/me/sakshisindhuja/messmate/datamodels/TestDataModel.kt
+++ app/src/main/java/me/sakshisindhuja/messmate/datamodels/TestDataModel.kt
@@ -6,15 +6,21 @@ class TestDataModel {
 
     fun loadMealPlans(): List<MealsPlan> {
         return listOf<MealsPlan>(
-            MealsPlan(1, "Masala Dosa", "Breakfast", 120),
-            MealsPlan(1, "Veg Biryani", "Lunch", 123),
-            MealsPlan(1, "Paneer Roti", "Dinner", 124),
-            MealsPlan(1, "Sambhar Vada", "Breakfast", 126),
-            MealsPlan(1, "Puri Chola", "Lunch", 128),
-            MealsPlan(1, "Bhindi Roti", "Dinner", 130),
-            MealsPlan(1, "Idli", "Breakfast", 131),
-            MealsPlan(1, "Fried Rice", "Lunch", 132),
-            MealsPlan(1, "Lachcha Paratha", "Dinner", 135),
+            MealsPlan(1, "Masala Dosa", "Breakfast 26/05", 120),
+            MealsPlan(2, "Veg Biryani", "Lunch 26/05", 123),
+            MealsPlan(3, "Paneer Roti", "Dinner 26/05", 124),
+            MealsPlan(4, "Sambhar Vada", "Breakfast 27/05", 126),
+            MealsPlan(5, "Puri Chola", "Lunch 27/05", 128),
+            MealsPlan(6, "Bhindi Roti", "Dinner 27/05", 130),
+            MealsPlan(7, "Idli", "Breakfast 28/05", 131),
+            MealsPlan(8, "Fried Rice", "Lunch 28/05", 132),
+            MealsPlan(9, "Lachcha Paratha", "Dinner 28/05", 135),
+            MealsPlan(10, "Fruit Salad", "Breakfast 28/05", 136),
+            MealsPlan(11, "Aalo, Aachar, Paratha", "Lunch 28/05", 137),
+            MealsPlan(12, "Gobi Sabzi", "Dinner 28/05", 138),
+            MealsPlan(13, "Poha", "Breakfast 28/05", 140),
+            MealsPlan(14, "Tadka, Roti, Omlet", "Lunch 28/05", 145),
+            MealsPlan(14, "Kaddu Roti", "Dinner 28/05", 151),
         )
     }
 }
