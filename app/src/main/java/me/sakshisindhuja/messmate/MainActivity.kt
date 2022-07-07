39a126af8f2d34afbbc66dbca3d674c8641e2a7e
Sakshi Sindhuja <sakshisindhuja.01@gmail.com>
2022-07-08 00:23:52 +0530

    UI and nav tweaks
    Added splash screen to nav.

--- app/src/main/java/me/sakshisindhuja/messmate/MainActivity.kt
+++ app/src/main/java/me/sakshisindhuja/messmate/MainActivity.kt
@@ -45,7 +45,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_splash, R.id.nav_home, R.id.nav_food, R.id.nav_complaint
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
